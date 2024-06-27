baf4666e307332cbc62af20b8bb7f2b2a2e9daa4
NotAlfred <mohammadalitalebi2001@gmail.com>
2024-06-27 09:29:06 +0330

    added write syscall

--- source/main.cpp
+++ source/main.cpp
@@ -33,9 +33,19 @@ main() -> int
     
     std::chrono::duration<float> delta_time_std23_print = time;
 
-    std::print("new printf took: {} s for 10000 iterations\n", delta_time_new_printf);
-    std::print("old printf took: {} s for 10000 iterations\n", delta_time_old_printf);
-    std::print("std23 print took: {} s for 10000 iterations\n", delta_time_std23_print);
+    loop_start = std::chrono::high_resolution_clock::now();
+    for (uint i = 0; i < 100000; ++i) {
+        write(STDOUT_FILENO, "hello, world! 120000-12\n\0", 25);
+    }
+    loop_end   = std::chrono::high_resolution_clock::now();
+    time       = loop_end - loop_start;
+    
+    std::chrono::duration<float> delta_time_write = time;
+
+    std::print("new printf took: {} for 10000 iterations\n", delta_time_new_printf);
+    std::print("old printf took: {} for 10000 iterations\n", delta_time_old_printf);
+    std::print("std23 print took: {} for 10000 iterations\n", delta_time_std23_print);
+    std::print("linux write syscall took: {} for 10000 iterations\n", delta_time_write);
 
     return 0;
 }
